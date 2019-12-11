67e101d7b205dd9c8e96b3df0c42c9b8a53f1bfb
MasterFire <quentin_gillet@orange.fr>
2019-12-11 15:12:37 +0100

    Add all traps (two don't work yet) Add text for indicate the players Minor update

--- tray.py
+++ tray.py
@@ -5,6 +5,8 @@ import time
 from goose import Goose
 from constants import *
 
+BLACK = (0, 0, 0)
+
 
 class Tray:
     background_image_src = ""
@@ -30,6 +32,10 @@ class Tray:
     player_count = 0
     player_turn = 0
     players_grid = [1, 1, 1, 1]
+    player_trap_state = [0, 0, 0, 0]
+
+    trap_grids = ((4, 0), (8, 1), (10, 2), (15, 0), (19, 2), (23, 1), (25, 3), (29, 2), (33, 4), (35, 0), (37, 5))
+    win_grid = 39
 
     window = None
 
@@ -41,19 +47,25 @@ class Tray:
         self.player_count = player_count
         self.first_space = True
         self.fast_dice_animation = False
+        self.is_keyboard_deactivate = False
         pygame.init()
+        self.font = pygame.font.Font(None, 40)
+        self.text_to_render = None
         self.create_window()
 
     def update(self):
         self.check_pygame_events()
         self.draw_players()
+        if self.text_to_render is not None:
+            self.window.blit(self.text_to_render, (130, 255))
+        pygame.display.flip()
 
     def check_pygame_events(self):
         for event in pygame.event.get():
             if event.type == QUIT:
                 self.run = False
                 pass
-            if event.type == KEYDOWN:
+            if event.type == KEYDOWN and not self.is_keyboard_deactivate:
                 if event.key == K_SPACE:
                     if self.first_space:
                         self.first_space = False
@@ -67,9 +79,10 @@ class Tray:
         pygame.display.set_caption("Jeu de l'oie")
         self.background = pygame.image.load(self.background_image_src).convert_alpha()
         self.window.blit(self.background, (0, 0))
-        pygame.display.flip()
         self.init_players()
         self.init_dices()
+        self.text_to_render = self.font.render("Appuiez sur espace pour lancer le dé.", 1, BLACK)
+        pygame.display.flip()
 
     def init_dices(self):
         self.dice_face1 = pygame.image.load(dice_face1_src).convert_alpha()
@@ -90,41 +103,103 @@ class Tray:
             self.players[k].init_sprite_positions()
 
     def launch_dices(self):
-        dice1_number = random.randint(1, 6)
-        dice2_number = random.randint(1, 6)
-        to_forward = dice1_number + dice2_number
-        self.animate_launch_dices(dice1_number, dice2_number)
-        self.forward_player(to_forward, self.player_turn)
+        if self.player_trap_state[self.player_turn] == 1:
+            self.text_to_render = self.font.render(
+                "Le joueur " + str((self.player_turn + 1)) + " est bloqué et doit attendre.", 1, BLACK)
+            time.sleep(1.2)
+            return
+        else:
+            dice1_number = random.randint(1, 6)
+            # dice2_number = random.randint(1, 6)
+            to_forward = dice1_number
+            self.animate_launch_dices(to_forward)
+            self.forward_player(to_forward, self.player_turn)
         if self.player_turn >= 3:
             self.player_turn = 0
         else:
             self.player_turn += 1
 
     def forward_player(self, to_forward, player_turn):
-        for i in range(0, self.player_count):
-            if i == player_turn:
-                self.players[i].move(players_possibles_positions[i][to_forward + self.players_grid[i]])
-                self.players_grid[i] += to_forward
+        self.text_to_render = self.font.render(
+            "Le joueur " + str((player_turn + 1)) + " avance de " + str(to_forward) + " case(s)", 1, BLACK)
+        for k in range(to_forward):
+            self.players[player_turn].move(players_possibles_positions[player_turn][k + self.players_grid[player_turn]])
+            time.sleep(0.3)
+            self.update()
+        self.players_grid[player_turn] += to_forward
+        for k in self.trap_grids:
+            if k[0] == self.players_grid[player_turn]:
+                if k[1] == 3 or k[1] == 5:
+                    for j in range(self.player_count):
+                        if self.player_trap_state[j] == 1 and self.players_grid[j] == self.players_grid[player_turn]:
+                            self.player_trap_state[j] = 0
+                            self.player_trap_state[player_turn] = 1
+                    return
+                else:
+                    self.trap_player(k[1], player_turn, to_forward)
+        self.is_keyboard_deactivate = False
 
-    def animate_launch_dices(self, dice1_number, dice2_number):
+    def backward_player(self, to_backward, player_turn):
+        self.text_to_render = self.font.render(
+            "Le joueur " + str((player_turn + 1)) + " recule de " + str(to_backward) + " case(s)", 1, BLACK)
+        time.sleep(0.4)
+        for k in range(to_backward):
+            self.players[player_turn].move(players_possibles_positions[player_turn][-k + (self.players_grid[player_turn] - 2)])
+            time.sleep(0.3)
+            self.update()
+        self.players_grid[player_turn] -= to_backward
+        self.is_keyboard_deactivate = False
+
+    def trap_player(self, trap, player_turn, previous_forward):
+        if trap == 0:
+            self.backward_player(3, player_turn)
+            pass
+        elif trap == 1:
+            time.sleep(0.4)
+            self.forward_player(3, player_turn)
+            pass
+        elif trap == 2:
+            time.sleep(0.4)
+            self.forward_player(previous_forward, player_turn)
+            pass
+        elif trap == 3:
+            self.player_trap_state[player_turn] = 1
+            time.sleep(0.4)
+            self.is_keyboard_deactivate = False
+            pass
+        elif trap == 4:
+            time.sleep(0.4)
+            self.players[player_turn].move(players_possibles_positions[player_turn][12])
+            self.update()
+            self.is_keyboard_deactivate = False
+            pass
+        elif trap == 5:
+            self.player_trap_state[player_turn] = 1
+            time.sleep(0.4)
+            self.is_keyboard_deactivate = False
+            pass
+
+    def animate_launch_dices(self, dice1_number, dice2_number=None):
         wait_time = 0.1
+        self.text_to_render = None
+        self.update()
         for k in range(0, 4):
             for i in range(0, 6):
                 if self.fast_dice_animation:
                     self.window.blit(self.dice_faces[dice1_number - 1], self.dices_pos[0])
-                    self.window.blit(self.dice_faces[dice2_number - 1], self.dices_pos[1])
+                    # self.window.blit(self.dice_faces[dice2_number - 1], self.dices_pos[1])
                     pygame.display.flip()
                     break
                 dice1 = self.dice_faces[i]
-                dice2 = self.dice_faces[i]
+                # dice2 = self.dice_faces[i]
                 if k == 3:
-                    if i <= dice1_number:
+                    if i <= dice1_number - 1:
                         self.window.blit(dice1, self.dices_pos[0])
-                    if i <= dice2_number:
-                        self.window.blit(dice2, self.dices_pos[1])
+                    # if i <= dice2_number:
+                    # self.window.blit(dice2, self.dices_pos[1])
                 else:
                     self.window.blit(dice1, self.dices_pos[0])
-                    self.window.blit(dice2, self.dices_pos[1])
+                    # self.window.blit(dice2, self.dices_pos[1])
                 pygame.display.flip()
                 self.check_pygame_events()
                 time.sleep(wait_time)
@@ -134,10 +209,10 @@ class Tray:
                 break
             pass
         time.sleep(1.5)
+        self.is_keyboard_deactivate = True
         self.first_space = True
 
     def draw_players(self):
         self.players_sprite.update()
         self.window.blit(self.background, (0, 0))
         self.players_sprite.draw(self.window)
-        pygame.display.flip()
